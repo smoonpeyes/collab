e3295aa815d794a3caea95c3a65edd9ddd45c207
Layla <smoonpeyes@gmail.com>
2013-12-01 16:41:58 -0700

    Added create and update CRUD actions.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,8 +8,33 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(params[:project])
+      if @project.save
+        flash[:notice] = "Project was saved."
+        redirect_to @project
+      else
+        flash[:error] = "There was an error saving the project. Please try again."
+        render :new
+      end
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project was updated."
+      redirect_to @project
+    else
+      flash[:error] = "There was an error saving the project. Please try again."
+      render :edit
+    end
+  end
+
 end
