497349abdc43af3f77588e6de7aad4831a96ac30
Layla <smoonpeyes@gmail.com>
2013-12-20 17:10:03 -0700

    Added Stripe fields to Sign Up form

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,2 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = "sk_test_VlcR4kMVCBOpaPot802Q8yRp"
+STRIPE_PUBLIC_KEY = "pk_test_aEuapFIWGGLkivDyhEmtADGi"
